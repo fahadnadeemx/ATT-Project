64a41e0aa60f9ab52667d7785330fc838a9dab28
fnadeem <fnadeem@pccube.com>
2023-01-23 05:20:51 +0100

    Controller Unit and Integration test

---
+++ src/test/java/com/book/management/controller/BookControllerTest.java
@@ -0,0 +1,130 @@
+package com.book.management.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.book.management.model.BookDto;
+import com.book.management.service.BookService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookControllerTest {
+    @InjectMocks
+    private BookController controller;
+
+    @Mock
+    private BookService service;
+
+    private BookDto book;
+    private Integer id;
+    private NoSuchElementException exception;
+
+    @Before
+    public void beforeClass() {
+        id = 1;
+        book = new BookDto();
+        exception = new NoSuchElementException("test exception");
+    }
+
+    @Test
+    public void testGetBooks() {
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        when(service.getBooks()).thenReturn(books);
+        List<BookDto> result = controller.getBooks();
+        verify(service).getBooks();
+        assertEquals(books, result);
+    }
+
+    @Test
+    public void testGetBooksEmptyList() {
+        List<BookDto> books = new ArrayList<>();
+        when(service.getBooks()).thenReturn(books);
+        List<BookDto> result = controller.getBooks();
+        verify(service).getBooks();
+        assertEquals(books, result);
+    }
+
+    @Test
+    public void testGetBook() {
+        when(service.getBook(any())).thenReturn(book);
+        BookDto result = controller.getBook(id);
+        verify(service).getBook(any());
+        assertEquals(book, result);
+    }
+
+    @Test
+    public void testGetBookNotFound() {
+        when(service.getBook(any())).thenThrow(exception);
+        NoSuchElementException result = assertThrows(NoSuchElementException.class, () -> controller.getBook(1));
+        verify(service).getBook(any());
+        assertEquals(exception.getMessage(), result.getMessage());
+    }
+
+    @Test
+    public void testSaveBook() {
+        when(service.saveBook(any())).thenReturn(book);
+        BookDto result = controller.saveBook(book);
+        verify(service).saveBook(any());
+        assertEquals(book, result);
+    }
+
+    @Test
+    public void testUpdateBook() {
+    	book.setId(id);
+        when(service.updateBook(any(), any())).thenReturn(book);
+        BookDto result = controller.updateBook(id, book);
+        verify(service).updateBook(any(), any());
+        assertEquals(book, result);
+    }
+
+    @Test
+    public void testUpdateBookNotFound() {
+    	book.setId(id);
+        when(service.updateBook(any(), any())).thenThrow(exception);
+        NoSuchElementException result = assertThrows(NoSuchElementException.class, () -> controller.updateBook(id, book));
+        verify(service).updateBook(any(), any());
+        assertEquals(exception.getMessage(), result.getMessage());
+    }
+
+    @Test
+    public void testUpdateBookDifferentId() {
+        book.setId(id + 1);
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> controller.updateBook(id, book));
+        verify(service, never()).updateBook(any(), any());
+        assertEquals(
+        	String.format("Id for url (%d) and dto (%d) must be the same", id, book.getId()),
+        	result.getMessage());
+    }
+
+    @Test
+    public void testDeleteBook() {
+        Boolean result = controller.deleteBook(id);
+        verify(service).deleteBook(any());
+        assertTrue(result);
+    }
+
+    @Test
+    public void testDeleteBookNotFound() {
+        doThrow(exception).when(service).deleteBook(any());
+        NoSuchElementException result = assertThrows(NoSuchElementException.class, () -> controller.deleteBook(id));
+        verify(service).deleteBook(any());
+        assertEquals(exception.getMessage(), result.getMessage());
+    }
+}
---
+++ src/test/java/com/book/management/controller/BookControllerTestIT.java
@@ -0,0 +1,260 @@
+package com.book.management.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.book.management.entity.BookEntity;
+import com.book.management.model.BookDto;
+import com.book.management.repository.BookRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class BookControllerTestIT {
+    private static final String URI = "/books";
+    private static final String URI_ID = URI + "/{id}";
+
+    @Autowired
+    private MockMvc request;
+
+    @Autowired
+    private BookRepository repository;
+
+    private BookEntity entity;
+    private BookDto dto;
+    private ObjectMapper mapper;
+
+    @Before
+    public void before() {
+        entity = new BookEntity(null,"nameEntity","authorEntity",2);
+        entity = repository.save(entity);
+        dto = new BookDto(null, "nameDto", "authorDto", 3);
+        mapper = new ObjectMapper();
+    }
+
+    @After
+    public void after() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void testGetBooks() throws Exception {
+        request.perform(get(URI))
+            .andDo(print())
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").exists(),
+                jsonPath("$").isArray(),
+                jsonPath("$.length()").value(1),
+                jsonPath("$[0].id").value(entity.getId()),
+                jsonPath("$[0].name").value(entity.getName()),
+                jsonPath("$[0].author").value(entity.getAuthor()),
+                jsonPath("$[0].price").value(entity.getPrice()));
+        assertEquals(1, repository.count());
+        BookEntity other = repository.findAll().get(0);
+        assertEquals(entity, other);
+    }
+
+    @Test
+    public void testGetBooksEmptyList() throws Exception {
+        repository.deleteAll();
+        request.perform(get(URI))
+            .andDo(print())
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").exists(),
+                jsonPath("$").isArray(),
+                jsonPath("$").isEmpty());
+        assertEquals(0, repository.count());
+    }
+
+    @Test
+    public void testGetBook() throws Exception {
+        request.perform(get(URI_ID, entity.getId()))
+            .andDo(print())
+            .andExpectAll(
+                status().isOk(),
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").exists(),
+                jsonPath("$").isMap(),
+                jsonPath("$").isNotEmpty(),
+                jsonPath("$.id").value(entity.getId()),
+                jsonPath("$.name").value(entity.getName()),
+                jsonPath("$.author").value(entity.getAuthor()),
+                jsonPath("$.price").value(entity.getPrice()));
+        assertEquals(1, repository.count());
+    }
+
+    @Test
+    public void testGetBookMissing() throws Exception {
+    	repository.deleteAll();
+    	assertEquals(0, repository.count());
+    	request.perform(
+    			get(URI_ID, entity.getId()))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isNotFound());
+    	assertEquals(0, repository.count());
+    }
+    
+    @Test
+    public void testSaveBook() throws Exception {
+    	repository.deleteAll();
+    	assertEquals(0, repository.count());
+    	request.perform(
+    			post(URI)
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isOk(),
+    			content().contentType(MediaType.APPLICATION_JSON),
+    			jsonPath("$").exists(),
+    			jsonPath("$").isMap(),
+    			jsonPath("$").isNotEmpty(),
+    			jsonPath("$.id").isNumber(),
+    			jsonPath("$.name").value(dto.getName()),
+    			jsonPath("$.author").value(dto.getAuthor()),
+    			jsonPath("$.price").value(dto.getPrice()));
+    	assertEquals(1, repository.count());
+    	BookEntity other = repository.findAll().get(0);
+    	assertEquals(dto.getName(), other.getName());
+    	assertEquals(dto.getAuthor(), other.getAuthor());
+    	assertEquals(dto.getPrice(), other.getPrice());
+    }
+
+    @Test
+    public void testSaveBookInvalid() throws Exception {
+    	repository.deleteAll();
+    	dto.setName("");
+    	assertEquals(0, repository.count());
+    	request.perform(
+    			post(URI)
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isBadRequest());
+    	assertEquals(0, repository.count());
+    }
+    
+    @Test
+    public void testUpdateBook() throws Exception {
+    	dto.setId(entity.getId());
+    	assertEquals(1, repository.count());
+    	request.perform(
+    			put(URI_ID, entity.getId())
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isOk(),
+    			content().contentType(MediaType.APPLICATION_JSON),
+    			jsonPath("$").exists(),
+    			jsonPath("$").isMap(),
+    			jsonPath("$").isNotEmpty(),
+    			jsonPath("$.id").isNumber(),
+    			jsonPath("$.name").value(dto.getName()),
+    			jsonPath("$.author").value(dto.getAuthor()),
+    			jsonPath("$.price").value(dto.getPrice()));
+    	assertEquals(1, repository.count());
+    	BookEntity other = repository.findAll().get(0);
+    	assertEquals(dto.getId(), other.getId());
+    	assertEquals(dto.getName(), other.getName());
+    	assertEquals(dto.getAuthor(), other.getAuthor());
+    	assertEquals(dto.getPrice(), other.getPrice());
+    }
+
+    @Test
+    public void testUpdateBookMissing() throws Exception {
+    	repository.deleteAll();
+    	dto.setId(entity.getId());
+    	assertEquals(0, repository.count());
+    	request.perform(
+    			put(URI_ID, entity.getId())
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isNotFound());
+    	assertEquals(0, repository.count());
+    }
+
+    @Test
+    public void testUpdateBookInvalidField() throws Exception {
+    	dto.setId(entity.getId());
+    	dto.setName("");
+    	assertEquals(1, repository.count());
+    	request.perform(
+    			put(URI_ID, entity.getId())
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isBadRequest());
+    	assertEquals(1, repository.count());
+    }
+
+    @Test
+    public void testUpdateBookDifferentId() throws Exception {
+    	dto.setId(entity.getId() + 1);
+    	assertEquals(1, repository.count());
+    	request.perform(
+    			put(URI_ID, entity.getId())
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isBadRequest());
+    	assertEquals(1, repository.count());
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+    	assertEquals(1, repository.count());
+    	request.perform(
+    			delete(URI_ID, entity.getId()))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isOk(),
+    			content().contentType(MediaType.APPLICATION_JSON),
+    			content().string("true"));
+    	assertEquals(0, repository.count());
+    }
+
+    @Test
+    public void testDeleteBookMissing() throws Exception {
+    	repository.deleteAll();
+    	dto.setId(entity.getId());
+    	assertEquals(0, repository.count());
+    	request.perform(
+    			delete(URI_ID, entity.getId())
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(mapper.writeValueAsString(dto)))
+    		.andDo(print())
+    		.andExpectAll(
+    			status().isNotFound());
+    	assertEquals(0, repository.count());
+    }
+}
