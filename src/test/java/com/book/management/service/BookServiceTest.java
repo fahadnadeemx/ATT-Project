13757d93f7f34fcbfa39bcc955fa0f0fc640e89b
fnadeem <fnadeem@pccube.com>
2023-01-29 00:49:00 +0100

    Book service test

--- src/test/java/com/book/management/service/BookServiceTest.java
+++ src/test/java/com/book/management/service/BookServiceTest.java
@@ -1,13 +1,11 @@
 package com.book.management.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
-import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -50,4 +48,98 @@ public class BookServiceTest {
 		assertEquals(2, bookList.size());
 	}
 
+	@Test
+	public void testGetBook_Success() {
+		// Arrange
+		Integer id = 1;
+		BookEntity book = new BookEntity();
+		when(repository.findById(id)).thenReturn(Optional.of(book));
+		BookDto expected = new BookDto();
+		when(mapper.mapToDto(book)).thenReturn(expected);
+
+		// Act
+		BookDto result = service.getBook(id);
+
+		// Assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testGetBook_NotFound() {
+		// Arrange
+		Integer id = 1;
+		when(repository.findById(id)).thenReturn(Optional.empty());
+
+		// Act and Assert
+		assertThrows(NoSuchElementException.class, () -> service.getBook(id));
+	}
+
+	@Test
+	public void testSaveBook() {
+		// Arrange
+		BookDto input = new BookDto();
+		BookEntity newBook = new BookEntity();
+		when(mapper.mapToEntity(input)).thenReturn(newBook);
+		BookEntity savedBook = new BookEntity();
+		when(repository.save(any(BookEntity.class))).thenReturn(savedBook);
+		BookDto expected = new BookDto();
+		when(mapper.mapToDto(savedBook)).thenReturn(expected);
+
+		// Act
+		BookDto result = service.saveBook(input);
+
+		// Assert
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void testUpdateBook_WhenExists() {
+		// Arrange
+		Integer id = 1;
+		BookDto input = new BookDto();
+		when(repository.existsById(id)).thenReturn(true);
+		BookEntity updatedBook = new BookEntity();
+		when(mapper.mapToEntity(input)).thenReturn(updatedBook);
+		when(repository.save(any(BookEntity.class))).thenReturn(updatedBook);
+		BookDto expected = new BookDto();
+		when(mapper.mapToDto(updatedBook)).thenReturn(expected);
+
+		// Act
+		BookDto result = service.updateBook(id, input);
+
+		// Assert
+		assertEquals(expected, result);
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testUpdateBook_WhenNotExists() {
+		// Arrange
+		Integer id = 1;
+		BookDto input = new BookDto();
+		when(repository.existsById(id)).thenReturn(false);
+
+		// Act
+		service.updateBook(id, input);
+	}
+
+	@Test
+	public void testDeleteBook_WhenExists() {
+		// Arrange
+		Integer id = 1;
+		when(repository.existsById(id)).thenReturn(true);
+		doNothing().when(repository).deleteById(id);
+
+		// Act
+		service.deleteBook(id);
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testDeleteBook_WhenNotExists() {
+		// Arrange
+		Integer id = 1;
+		when(repository.existsById(id)).thenReturn(false);
+
+		// Act
+		service.deleteBook(id);
+	}
 }
